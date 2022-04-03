d61939ba373317bd27bd49977a7fc912aec95c89
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-03 17:02:48 +0800

    deno task dev

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # hello-world-deno-deploy
 
 https://deno.com/deploy/docs/hello-world
+
+# 运行
+
+```
+deno task dev
+```
--- main.tsx
+++ main.tsx
@@ -1,14 +1,16 @@
-import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.133.0/http/server.ts";
 
 function handler(req: Request): Response {
+    const { url, headers } = req;
 
-
-  const { url, headers } = req
-
-  return new Response(
-    JSON.stringify(
-      ["Hello world", { url, headers: Object.fromEntries(headers) }]), { headers: { "content-type": "application/json" } });
+    return new Response(
+        JSON.stringify([
+            "Hello world",
+            { url, headers: Object.fromEntries(headers) },
+        ]),
+        { headers: { "content-type": "application/json" } },
+    );
 }
 
 console.log("Listening on http://localhost:8000");
-await serve(handler);
+await serve(handler, { port: 8000 });
