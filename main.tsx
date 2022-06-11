666fa6de4771fe52d2f1bb1e653781ccd829b478
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-11 14:26:54 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,22 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { ConnInfo, serve } from "https://deno.land/std@0.143.0/http/server.ts";
 
-function handler(req: Request): Response {
+function handler(req: Request, connInfo: ConnInfo): Response {
     const { url, headers, method } = req;
 
-    return new Response(
-        JSON.stringify(
-            { url, method, headers: Object.fromEntries(headers) },
-        ),
-        { headers: { "content-type": "application/json" } },
-    );
+    const data = {
+        connInfo,
+        url,
+        method,
+        headers: Object.fromEntries(headers),
+    };
+    console.log(data);
+    return new Response(JSON.stringify(data, null, 4), {
+        headers: { "content-type": "application/json" },
+    });
 }
 
 console.log("Listening on http://localhost:8000");
-await serve(handler, { port: 8000 });
+await Promise.all([
+    serve(handler, { port: 8000 }),
+    serve(handler, { hostname: "::", port: 8000 }),
+]);
