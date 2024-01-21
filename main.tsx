cf2cc2999261cf7dafd95b94a49914b57fccd268
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-22 02:06:39 +0800

    同时监听ipv6 和ipv4

--- main.tsx
+++ main.tsx
@@ -1,33 +1,19 @@
-import { ConnInfo, serve } from "https://deno.land/std@0.182.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 import { parse } from "https://deno.land/std@0.182.0/flags/mod.ts";
-export function handler(req: Request, connInfo: ConnInfo): Response {
-    const { url, headers, method } = req;
-
-    const data = {
-        ...connInfo,
-        url,
-        method,
-        headers: Object.fromEntries(headers),
-    };
-
-    const body = JSON.stringify(data);
-    //     console.log("request", body);
-    return new Response(body, {
-        headers: {
-            "Strict-Transport-Security": "max-age=31536000",
-            "content-type": "application/json",
-        },
-    });
-}
+import { handler } from "./handler.tsx";
 if (import.meta.main) {
-    let { port, hostname } = parse(Deno.args);
-    if (port || hostname) {
-        port ??= 8000;
-        hostname ??= "0.0.0.0";
-        await serve(handler, { port, hostname });
-    } else {
-        //         console.log("Listening on http://localhost:8000");
+  let { port, hostname } = parse(Deno.args);
+  if (port || hostname) {
+    port ??= 8000;
+    hostname ??= "0.0.0.0";
+    await serve(handler, { port, hostname });
+  } else {
+    //         console.log("Listening on http://localhost:8000");
 
-        await serve(handler, { hostname: "0.0.0.0", port: 8000 });
-    }
+    // await serve(handler, { hostname: "0.0.0.0", port: 8000 });
+    await Promise.all([
+      serve(handler, { hostname: "::", port: 8000 }),
+      serve(handler, { hostname: "0.0.0.0", port: 8000 }),
+    ]);
+  }
 }
