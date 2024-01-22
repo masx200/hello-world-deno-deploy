767d18401cde7972998c37f05fafbbe955dabcf0
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-22 22:34:24 +0800

    Update handler.tsx

--- handler.tsx
+++ handler.tsx
@@ -4,20 +4,31 @@ export function handler(req: Request, connInfo: ConnInfo): Response {
   const { url, headers, method } = req;
 
   const data = {
-    ...connInfo,
-    url,
-    method,
-    headers: Object.fromEntries(headers),
+    connInfo,
+    request: {
+      url,
+      method,
+      headers: Object.fromEntries(headers),
+    },
   };
 
   const body = JSON.stringify(data);
-  console.log("request", data);
+  // console.log("request", data);
   const response = new Response(body, {
     headers: {
       "Strict-Transport-Security": "max-age=31536000",
       "content-type": "application/json",
     },
   });
-  console.log(response, { url, method });
+  // console.log(response, { url, method });
+
+  console.log({
+    connInfo,
+    request: { url, method, headers: Object.fromEntries(headers) },
+    response: {
+      status: response.status,
+      headers: Object.fromEntries(response.headers),
+    },
+  });
   return response;
 }
