8c57ccc3ac8d9aa9a3c37d52c06481ff5928fd90
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-26 12:46:55 +0800

    response_headers

--- handler.tsx
+++ handler.tsx
@@ -2,7 +2,11 @@ import { ConnInfo } from "https://deno.land/std@0.182.0/http/server.ts";
 
 export function handler(req: Request, connInfo: ConnInfo): Response {
     const { url, headers, method } = req;
-
+    const response_headers = {
+        "Strict-Transport-Security": "max-age=31536000",
+        "content-type": "application/json",
+    };
+    const status = 200;
     const data = {
         connInfo,
         request: {
@@ -10,15 +14,14 @@ export function handler(req: Request, connInfo: ConnInfo): Response {
             method,
             headers: Object.fromEntries(headers),
         },
+        response: { status: status, headers: response_headers },
     };
 
     const body = JSON.stringify(data);
     // console.log("request", data);
     const response = new Response(body, {
-        headers: {
-            "Strict-Transport-Security": "max-age=31536000",
-            "content-type": "application/json",
-        },
+        status: status,
+        headers: response_headers,
     });
     // console.log(response, { url, method });
 
